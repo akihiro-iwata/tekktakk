a3b12a51ff55d0e1dda1b700ba967306c31f76f0
akihiro iwata <akihiro-iwata@m3.com>
2019-10-23 20:45:14 +0900

    remove footer

--- pages/index.vue
+++ pages/index.vue
@@ -60,7 +60,7 @@ export default {
 <style scoped lang="scss">
 .index-container {
   width: 100vw;
-  height: 95vh;
+  height: 100vh;
   margin: 0;
   padding: 0;
   background-image: url("~assets/img/top.jpg");
